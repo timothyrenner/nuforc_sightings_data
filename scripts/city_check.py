496689a890dc23152925324f4fd03501ae854cda
Tim Renner <renner.timothy@gmail.com>
2017-09-06 21:56:54 -0500

    Added count to city check for easier prioritization.

--- scripts/city_check.py
+++ scripts/city_check.py
@@ -2,18 +2,27 @@
 
 import pandas as pd
 from csv import DictReader, DictWriter
-from toolz import compose, pluck
+from toolz import compose, pluck, countby
 import sys
 
 sightings = DictReader(open('data/processed/nuforc_reports.csv', 'r'))
-bad_cities = \
-    DictWriter(open('bad_cities.csv', 'w'), fieldnames=['city','state'])
+bad_cities_file = \
+    DictWriter(open('bad_cities.csv', 'w'), fieldnames=['city','state','count'])
 
+bad_cities = []
 for sighting in sightings:
     if not sighting['city_latitude'] or not sighting['city_longitude']:
-        bad_cities.writerow(
+        bad_cities.append(
             {
                 "city": sighting["city"],
                 "state": sighting["state"]
             }
         )
+
+bad_cities_counts = countby(['state','city'], bad_cities)
+
+for (state, city), count in \
+    sorted(bad_cities_counts.items(), key=lambda x: x[1], reverse=True):
+    
+    bad_cities_file.writerow({'state': state, 'city': city, 'count': count})
+    
